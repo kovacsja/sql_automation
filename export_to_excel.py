52aa960302fda0bfb40bf3ec6d29b974840961e7
kovacsja <janos.kovacs.h@gmail.com>
2021-03-13 16:54:59 +0100

    win_auth

--- export_to_excel.py
+++ export_to_excel.py
@@ -2,28 +2,34 @@ import os
 import pyodbc
 import pandas as pd
 from getpass import getpass
-import tqdm #TODO: progressbar
+
+# import tqdm  # TODO: progressbar
 
 SCRIPT_DIR: str = "sql_to_excel/sripts"
-
-pw = getpass("Password?")
 
 conn_params = {
     "driver": "ODBC Driver 17 for SQL Server",
     "server": "globalfr",
     "db": "AdventureWorks2017",
     "me": "SA",
-    "passw": pw,
 }
+
 
 scripts = [i for i in os.listdir(SCRIPT_DIR)]
 
-conn = pyodbc.connect(
-    f"Driver={conn_params['driver']};SERVER={conn_params['server']};Database={conn_params['db']};UID={conn_params['me']};PWD={conn_params['passw']}"
-)
+pw = getpass("Password?")
 
-#TODO: connection tesztelése (ping?)
-#TODO: sql_alchemy variáns connection készítése
+if pw:
+    conn = pyodbc.connect(
+        f"Driver={conn_params['driver']};SERVER={conn_params['server']};Database={conn_params['db']};UID={conn_params['me']};PWD={pw}"
+    )
+else:
+    conn = pyodbc.connect(
+        f"Driver={conn_params['driver']};SERVER={conn_params['server']};Database={conn_params['db']};Trusted_Connection=yes"
+    )
+
+# TODO: connection tesztelése (ping?)
+# TODO: sql_alchemy variáns connection készítése
 
 with pd.ExcelWriter("data.xlsx") as ew:
     for s in scripts:
